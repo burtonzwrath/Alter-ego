477fbcb4bd0f79c2ee854eef2e317b7fc9810dd7
burtonzwrath <burtonzwrath@gmail.com>
2023-02-20 13:48:15 +0200

    pages create

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
+import SharedLayout from "./features/ui/sharedLyout/SharedLayout";
+import HomePage from "./pages/HomePage";
+import NewsPage from "./pages/NewsPage";
+import ProfilePage from "./pages/ProfilePage";
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
       <>
         <Routes>
           <Route path="/" element={<SharedLayout />}>
-            <Route index element={<Home/>} />
-            <Route path="news/*" element={<News />} />
-            <Route path="profile/*" element={<Profile />} />
+            <Route index element={<HomePage />} />
+            <Route path="news/*" element={<NewsPage />} />
+            <Route path="profile/*" element={<ProfilePage />} />
           </Route>
         </Routes>
       </>
---
+++ src/features/ui/sharedLyout/SharedLayout.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SharedLayout(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default SharedLayout;
