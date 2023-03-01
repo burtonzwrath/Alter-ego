791676cf5b92de54cf9917efc8fe974106e25461
burtonzwrath <burtonzwrath@gmail.com>
2023-03-01 15:22:59 +0200

    home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,10 +2,22 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setFlag } from "../redux/news/newsSlice";
 import { getNews } from "../redux/news/selectors/selectors";
+import {Box, Paper, Typography} from "@mui/material";
+import ukraine from "../assets/image/Ukraine.jpg";
+import {makeStyles} from "@mui/styles";
+
+const styles = makeStyles({
+  homeImg: {
+    borderRadius:"20px"
+  }
+})
+
+
 
 const HomePage = () => {
   const news = useSelector(getNews);
   const dispatch = useDispatch();
+  const s = styles()
 
   useEffect(() => {
     if (news.length !== 0) {
@@ -13,6 +25,11 @@ const HomePage = () => {
     }
   }, [dispatch]);
 
-  return <div>HomePage</div>;
+  return (
+      <Box>
+       <Typography align="center" variant="h1"  color="common.white">Ukraine is my home</Typography>
+          <img className={s.homeImg} src={ukraine} alt="ukraine" />
+      </Box>
+  );
 };
 export default HomePage;
