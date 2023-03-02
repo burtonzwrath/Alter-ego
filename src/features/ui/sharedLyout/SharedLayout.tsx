a6e530015cce0c5a413bc83300baba5d78524ce4
burtonzwrath <burtonzwrath@gmail.com>
2023-03-02 12:40:55 +0200

    styles sharedLayout

--- src/features/ui/sharedLyout/SharedLayout.tsx
+++ src/features/ui/sharedLyout/SharedLayout.tsx
@@ -3,22 +3,10 @@ import { Outlet } from "react-router";
 import NavBar from "../../components/navBar/NavBar";
 import { makeStyles } from "@mui/styles";
 import { Box } from "@mui/material";
-
-const styles = makeStyles({
-  container: {
-    border: "13px solid black",
-    backgroundColor: "burlywood",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    minHeight: "800px",
-    maxWidth: "1400px",
-    margin: "0 auto",
-  },
-});
+import { sharedLayoutStyles } from "./sharedLayoutStyles";
 
 function SharedLayout() {
-  const s = styles();
+  const s = sharedLayoutStyles();
 
   return (
     <Box className={s.container}>
