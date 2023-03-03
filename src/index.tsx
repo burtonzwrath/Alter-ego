8483c2b867d1cdf95055ea937be0adfda91f5760
burtonzwrath <burtonzwrath@gmail.com>
2023-03-03 12:54:57 +0200

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
-import React from "react";
+import { HashRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "./i18next";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("root")
 );
